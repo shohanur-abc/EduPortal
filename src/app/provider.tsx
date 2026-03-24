b012d43be91c85bb9ccd99589d89fb5857a768bd
shohan <shohanur7@proton.me>
2026-03-24 20:58:02 +0600

    refactor: update root app layout, provider, and styles

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,17 +1,22 @@
+"use client"
+
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { Toaster } from "@/components/ui/sonner"
 import { ThemeProvider } from "next-themes"
+import { NuqsAdapter } from "nuqs/adapters/next/app"
 // import { SessionProvider } from "next-auth/react"
 
 export default function Provider({ children }: { children: React.ReactNode }) {
     return (
         //<SessionProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <TooltipProvider>
-                {children}
-                <Toaster />
-            </TooltipProvider>
-        </ThemeProvider>
+        <NuqsAdapter>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                <TooltipProvider>
+                    {children}
+                    <Toaster />
+                </TooltipProvider>
+            </ThemeProvider>
+        </NuqsAdapter>
         //</SessionProvider>
     )
 }
